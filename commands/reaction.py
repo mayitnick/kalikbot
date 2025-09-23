bb82d8d98a56107145f16c2849cddb842dc95334
VayLorm <123010340+VayLorm@users.noreply.github.com>
2025-09-23 20:22:11 +0300

    Пофиксили немного

--- commands/reaction.py
+++ commands/reaction.py
@@ -2,33 +2,8 @@ from telebot import TeleBot
 from telebot.types import Message
 import modules.permissions as permissions
 from modules.constants import CONSTANTS
+import modules.react as react
 import database
-import requests
-import dotenv
-import os
-from random import choice
-
-dotenv.load_dotenv()
-TOKEN = os.getenv("TOKEN")
-
-
-def send_react(chat_id, message_id, emoji):
-    global TOKEN
-    
-    url = f'https://api.telegram.org/bot{TOKEN}/setMessageReaction'
-    data = {
-        'chat_id': chat_id,
-        'message_id': message_id,
-        'reaction': [
-            {
-                'type': 'emoji',
-                'emoji': emoji
-            }
-        ],
-        'is_big': False
-    }
-    response = requests.post(url, json=data)
-    result = response.json()
 
 ALIASES = ["харофий", "хороший", "харош", "хароший", "добрий", "добрый", "лютый", "имба", "лега", "легенда"]
 
@@ -40,6 +15,6 @@ def handle(
     CONSTANTS: CONSTANTS,
     FOUNDER_ID: int,) -> bool:
     
-    send_react(message.chat.id, message.message_id, "🔥")
+    react.send_react(message.chat.id, message.message_id, "🔥")
     return True  # сигнал, что команда сработала
     # я хезе, это не везде есть, но мне в падлу это проверять :3

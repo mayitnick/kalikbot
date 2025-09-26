9b59158991ab686cf1c7fbe624b2f4c8dcc8c73f
VayLorm <123010340+VayLorm@users.noreply.github.com>
2025-09-26 22:05:28 +0300

    Пару фиксов для тестов, начало разработки "дежурства"

---
+++ commands/duty_info.py
@@ -0,0 +1,60 @@
+from telebot import TeleBot
+from telebot.types import Message
+import modules.permissions as permissions
+from modules.constants import CONSTANTS
+import database
+import random
+
+# Временное решение, нужно будет это потом в отдельный модуль вынести ;)
+def get_url_from_id(full_name, id):
+    # [Имя](tg://user?id=123456789)
+    return f"[{full_name}](tg://user?id={id})"
+
+# Ещё одно временное решение)
+def if_reply_to_message(message, user_id, db):
+    if message.reply_to_message:
+        reply_to_message_id = message.reply_to_message.from_user.id
+        return db.get_user_by_id(reply_to_message_id), 1
+    else:
+        return db.get_user_by_id(int(user_id)), 0
+
+ALIASES = ["дежурство"]
+
+def handle(
+    message: Message,
+    bot: TeleBot,
+    db: database.Database,
+    perm: permissions.Permissions,
+    CONSTANTS: CONSTANTS,
+    FOUNDER_ID: int,) -> bool:
+    parts = message.text.split()
+    
+    try:
+        if message.reply_to_message:
+            user_id = message.reply_to_message.from_user.id
+        else:
+            user_id = parts[2]
+        author = db.get_user_by_id(message.from_user.id)
+        if author:
+            if perm.check_for_permissions(author["type"], f"see.duty") or message.from_user.id == FOUNDER_ID:
+                # Дополнительная проверка на реплай
+                user, is_reply = if_reply_to_message(message, user_id, db)
+                if user:
+                    full_name = user["full_name"]
+                    duty_info = db.get_duty_info(user_id)
+                    if not duty_info:
+                        db.setup_duty_info(user_id)
+                    
+                    if duty_info["last_duty"]:
+                        bot.reply_to(message, f"✅ {get_url_from_id(full_name, user_id)} был дежурным {duty_info['last_duty']}")
+                    else:
+                        bot.reply_to(message, f"❌ {get_url_from_id(full_name, user_id)} не был дежурным. ")
+                    
+                    return
+            else:
+                bot.reply_to(message, random.choice(CONSTANTS["kalik_noperm"]))
+                return
+    except:
+        bot.reply_to(message, '❌ Ошибка! Нужно написать в таком формате: "Калик, о нём 12345678". Вместо 12345678 надо указать айди студента в ТГ ("Калик, айди")') 
+    return True  # сигнал, что команда сработала
+    # я хезе, это не везде есть, но мне в падлу это проверять :3
--- database.py
+++ database.py
@@ -245,3 +245,55 @@ class Database:
         else:
             print("Расписание не найдено.")
             return None
+    def setup_duty_info(self, id):
+        if id in [user["id"] for user in self.data["users"]]:
+            user = self.get_user_by_id(id)
+            if user["duty_info"] is None:
+                user["duty_info"] = {
+                    "last_duty": None,
+                    "amount_of_duties": None,
+                    "pair_id": None,
+                    "preferences": None
+                }
+                self.save()
+    def set_last_duty(self, id, last_duty):
+        if id in [user["id"] for user in self.data["users"]]:
+            user = self.get_user_by_id(id)
+            user["duty_info"]["last_duty"] = last_duty
+            self.save()
+            print(f"Последний дежурный для пользователя {id} установлен.")
+            return True
+    def set_amount_of_duties(self, id, amount_of_duties):
+        if id in [user["id"] for user in self.data["users"]]:
+            user = self.get_user_by_id(id)
+            user["duty_info"]["amount_of_duties"] = amount_of_duties
+            self.save()
+            print(f"Количество дежурств для пользователя {id} установлено.")
+            return True
+    def add_amount_of_duties(self, id):
+        if id in [user["id"] for user in self.data["users"]]:
+            user = self.get_user_by_id(id)
+            if user["duty_info"]["amount_of_duties"] is not None:
+                user["duty_info"]["amount_of_duties"] += 1
+                self.save()
+                print(f"Количество дежурств для пользователя {id} увеличено.")
+                return True
+    def set_pair_id(self, id, pair_id):
+        if id in [user["id"] for user in self.data["users"]]:
+            user = self.get_user_by_id(id)
+            user["duty_info"]["pair_id"] = pair_id
+            self.save()
+            print(f"Пара для пользователя {id} установлена.")
+            return True
+    def add_to_preferences(self, id, pair_id):
+        if id in [user["id"] for user in self.data["users"]]:
+            user = self.get_user_by_id(id)
+            if user["duty_info"]["preferences"] is None:
+                user["duty_info"]["preferences"] = []
+            if pair_id not in user["duty_info"]["preferences"]:
+                user["duty_info"]["preferences"].append(pair_id)
+            self.save()
+    def get_duty_info(self, id):
+        if id in [user["id"] for user in self.data["users"]]:
+            user = self.get_user_by_id(id)
+            return user["duty_info"]

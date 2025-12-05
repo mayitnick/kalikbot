bc1e03ae3d919111b407f458d1ef8faa947ff15d
VayLorm <123010340+VayLorm@users.noreply.github.com>
2025-12-05 09:56:49 +0300

    косметическое исправления

--- commands/time.py
+++ commands/time.py
@@ -15,7 +15,7 @@ def get_time_until_target_simple():
         target += timedelta(days=1)
     diff = target - now
     s = int(diff.total_seconds())
-    return f"{s // 3600} ч {(s % 3600) // 60} м"
+    return f"{s // 3600}ㅤчㅤ{(s % 3600) // 60}ㅤм"
 
 def handle(
     message: Message,
@@ -26,7 +26,7 @@ def handle(
     FOUNDER_ID: int,) -> bool:
 
     bot.reply_to(message, 
-        f"```Осталось: {get_time_until_target_simple()}\n1. 8:20 - 9:05\n2. 9:05 - 9:50\n3. 10:00 - 10:45\n4. 10:45 - 11:30\n5. 11:35 - 12:20\n6. 12:25 - 13:10\n7. 13:15 - 14:00\n8. 14:00 - 14:45\n9. 14:50 - 15:35\n10. 15:40 - 16:25\n```", parse_mode="MARKDOWNV2")
+        f"```Осталось:ㅤ{get_time_until_target_simple()}\n1. 8:20 - 9:05\n2. 9:05 - 9:50\n3. 10:00 - 10:45\n4. 10:45 - 11:30\n5. 11:35 - 12:20\n6. 12:25 - 13:10\n7. 13:15 - 14:00\n8. 14:00 - 14:45\n9. 14:50 - 15:35\n10. 15:40 - 16:25\n```", parse_mode="MARKDOWNV2")
     
     
     return True  # сигнал, что команда сработала

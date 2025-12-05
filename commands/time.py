d64de697e7518e3650afac40f5086a44ca4f0187
VayLorm <123010340+VayLorm@users.noreply.github.com>
2025-12-05 09:37:05 +0300

    Прикол с командой "Кали время"

--- commands/time.py
+++ commands/time.py
@@ -3,9 +3,19 @@ from telebot.types import Message
 import modules.permissions as permissions
 from modules.constants import CONSTANTS
 import database
-import datetime
+from datetime import datetime, timedelta
 
 ALIASES = ["время"]
+
+def get_time_until_target_simple():
+    now = datetime.now()
+    h, m = (16, 25) if now.weekday() == 0 else (14, 45)
+    target = now.replace(hour=h, minute=m, second=0, microsecond=0)
+    if now > target:
+        target += timedelta(days=1)
+    diff = target - now
+    s = int(diff.total_seconds())
+    return f"{s // 3600} часов {(s % 3600) // 60} минут {s % 60} секунд"
 
 def handle(
     message: Message,
@@ -14,9 +24,9 @@ def handle(
     perm: permissions.Permissions,
     CONSTANTS: CONSTANTS,
     FOUNDER_ID: int,) -> bool:
-    now = datetime.datetime.now()
-    ctime = now.strftime("%H:%M:%S")
 
-    bot.reply_to(message, f"Я думаю, что сейчас {ctime}")
+    bot.reply_to(message, f"До конца пар осталось: {get_time_until_target_simple}")
+    
+    
     return True  # сигнал, что команда сработала
     # я хезе, это не везде есть, но мне в падлу это проверять :3
